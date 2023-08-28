0178e6900db5e94f9bc67b71361572c445f70dd8
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2023-08-28 15:24:42 +0200

    feat: handle formidable errors

--- server/utils/safeResponseHandler.ts
+++ server/utils/safeResponseHandler.ts
@@ -1,6 +1,7 @@
 import { PrismaClientValidationError } from '@prisma/client/runtime/library'
 import type { EventHandler, H3Event } from 'h3'
 import { ValidationError } from 'yup'
+import { errors as formidableErrors } from 'formidable';
 
 export const safeResponseHandler = (handler: EventHandler) =>
   defineEventHandler(async (event: H3Event) => {
@@ -9,7 +10,7 @@ export const safeResponseHandler = (handler: EventHandler) =>
       const response = await handler(event)
       // do something after the route handler
       return response
-    } catch (err) {
+    } catch (err: any) {
       let msg = 'Unexpected server error';
       let status = event.res.statusCode === 200 ? 500 : event.res.statusCode;
       let longMsg;
@@ -21,9 +22,15 @@ export const safeResponseHandler = (handler: EventHandler) =>
         status = 400;
         msg = 'Database error. Your input values can probably not be written to database.';
         longMsg = err.message
+      } else if (err?.name === 'Error' && Object.values(formidableErrors).includes(err?.code)) {
+        status = 400;
+        if (err?.message) msg = err?.message;
+        if (err?.httpCode) status = err?.httpCode;
       } else {
         // Error handling
         console.error('----------------  Safe response handler caught error! --------------')
+        console.error('NAME:', err?.name)
+        console.error('KEYS:', Object.keys(err))
         console.error(err)
         console.error('----------------     -     ---------------     -     ---------------');
       }
