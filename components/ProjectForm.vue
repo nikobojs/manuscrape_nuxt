6b37e0f61faf74b101c0269c8b486f25d8e5e4a5
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2023-09-20 11:02:06 +0200

    fix: improve observation form handling of checkbox + ux improvements

--- components/ProjectForm.vue
+++ components/ProjectForm.vue
@@ -1,54 +1,54 @@
 <template>
   <h2 class="text-3xl mb-3">Create project</h2>
-  <UCard>
-  <form @submit.prevent="handleSubmit">
+  <UCard class="overflow-visible">
+    <form @submit.prevent="handleSubmit">
 
-    <div class="flex gap-x-8 items-start">
-      <div class="w-80 flex gap-3 flex-col">
-        <label for="name-input">
-          Name:
-        </label>
-        <UInput
-          v-model="form.name"
-          placeholder="Enter project name"
-          id="name-input"
-          required
-        />
-        <label class="mt-5" for="field-label-input">
-          Observation fields:
-        </label>
-        <UInput v-model="fieldLabel"  id="field-label-input" placeholder="Enter field label" />
-        <USelectMenu v-model="fieldType" :options="fieldTypeOptions" placeholder="Select field type" />
-        <div class="grid grid-cols-2 mt-1 w-full">
-          <UCheckbox class="items-center" v-model="fieldRequired" label="Field is required" />
-          <div class="w-full text-right">
-            <UButton icon="i-heroicons-plus" variant="outline" color="blue" type="button" @click="addField">
-              Add field
-            </UButton>
+      <div class="flex gap-x-8 items-start">
+        <div class="w-80 flex gap-3 flex-col">
+          <label for="name-input">
+            Name:
+          </label>
+          <UInput
+            v-model="form.name"
+            placeholder="Enter project name"
+            id="name-input"
+            required
+          />
+          <label class="mt-5" for="field-label-input">
+            Observation fields:
+          </label>
+          <UInput v-model="fieldLabel"  id="field-label-input" placeholder="Enter field label" />
+          <USelectMenu v-model="fieldType" :options="fieldTypeOptions" placeholder="Select field type" />
+          <div class="grid grid-cols-2 mt-1 w-full">
+            <UCheckbox class="items-center" v-model="fieldRequired" :disabled="forceFieldRequired" label="Field is required" />
+            <div class="w-full text-right">
+              <UButton icon="i-heroicons-plus" variant="outline" color="blue" type="button" @click="addField">
+                Add field
+              </UButton>
+            </div>
+          </div>
+        </div>
+        <div class="w-96">
+          <div v-if="addedFields.length > 0" class="w-full">
+            <label class="block mb-2">Fields added to project:</label>
+            <div v-for="field in addedFields" class="grid grid-cols-3 gap-x-4 w-full">
+              <span class="">{{ field.label }}</span>
+              <span class="text-gray-500 text-right">{{ field.field.label }}</span>
+              <span class="text-gray-500 text-right">{{ field.required ? 'Yes' : 'No' }}</span>
+            </div>
           </div>
         </div>
       </div>
-      <div class="w-96">
-        <div v-if="addedFields.length > 0" class="w-full">
-          <label class="block mb-2">Fields added to project:</label>
-          <div v-for="field in addedFields" class="grid grid-cols-2 gap-x-4 w-full">
-            <span class="">{{ field.label }}</span>
-            <span class="text-gray-500 text-right">{{ field.field.label }}</span>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <br />
-    <br />
+      <br />
+      <br />
 
-    <UButton type="submit" :loading="loading">
-      Create project
-    </UButton>
-    <span v-text="error" class="block mt-3 text-red-600"></span>
-  </form>
+      <UButton type="submit" :loading="loading">
+        Create project
+      </UButton>
+      <span v-text="error" class="block mt-3 text-red-600"></span>
+    </form>
   </UCard>
-  
 </template>
 
 <script setup lang="ts">
@@ -60,6 +60,18 @@
   const toast = useToast();
 
   const fieldType = ref(undefined as NewField | undefined);
+  const forceFieldRequired = computed(() => {
+    if (
+      fieldType.value &&
+      ['BOOLEAN'].includes(fieldType.value?.type)
+    ) {
+      fieldRequired.value = true;
+      return true;
+    } else {
+      fieldRequired.value = false;
+      return false;
+    }
+  })
 
   const fieldTypes: Record<string, string> = {
     'Text': 'STRING',
@@ -85,7 +97,7 @@
     fields: [],
   } as NewProjectBody);
 
-  const addedFields = ref([] as { label: string; field: NewField }[])
+  const addedFields = ref([] as { label: string; field: NewField, required: boolean }[])
 
   async function handleSubmit() {
     loading.value = true;
@@ -135,7 +147,8 @@
 
     addedFields.value.push({
       label: fieldLabel.value,
-      field: fieldType.value
+      field: fieldType.value,
+      required: fieldRequired.value,
     })
 
     fieldLabel.value = '';
