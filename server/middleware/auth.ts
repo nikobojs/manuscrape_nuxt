d9effeb12eb57495204b959c4d18802d5404a1ac
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2026-02-06 08:26:51 +0100

    fix: missing drizzle refactor in auth middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,63 +1,61 @@
-import jwt from 'jsonwebtoken';
-import type { H3Event, EventHandlerRequest } from 'h3';
+import jwt from "jsonwebtoken";
+import type { H3Event, EventHandlerRequest } from "h3";
 
 const config = useRuntimeConfig();
 
 // TODO: refactor and improve readability
 export default defineEventHandler(async (event) => {
-    const cookieValue = getCookie(event, 'authcookie');
-    const headers = getHeaders(event);
-    const authToken = headers.authentication || cookieValue;
-    const params = getRouterParams(event);
-    let loginSuccesfull = false;
+  const cookieValue = getCookie(event, "authcookie");
+  const headers = getHeaders(event);
+  const authToken = headers.authentication || cookieValue;
+  console.log("got authTOken:L", authToken);
+  let loginSuccesfull = false;
 
-    if (!authToken && !isOpenUrl(event)) {
-        return onNotAuthed(event);
-    }
-
-    else if (typeof authToken == 'string' && authToken.length > 0) {
-        try {
-            const decoded = jwt.verify(authToken, config.tokenSecret);
-            if (typeof decoded !== 'string' && decoded?.id) {
-                const user = await db.user.findFirst({
-                    where: { id: decoded.id },
-                    select: {...bigUserQuery, password: true},
-                });
-
-                if (user) {
-                    loginSuccesfull = true;
-                    event.context.user = user as CurrentUser;
-                } else {
-                    return onNotAuthed(event, 'Session is valid but user does not exist')
-                }
-            } else {
-                return onNotAuthed(event, 'Your did not provide any authorization ');
-            }
-        } catch(e) {
-            return onNotAuthed(event, 'Malformed JWT');
+  if (!authToken && !isOpenUrl(event)) {
+    return onNotAuthed(event);
+  } else if (typeof authToken == "string" && authToken.length > 0) {
+    try {
+      const decoded = jwt.verify(authToken, config.tokenSecret);
+      if (typeof decoded !== "string" && decoded?.id) {
+        const user = await getFullUserById(decoded.id);
+        if (user) {
+          loginSuccesfull = true;
+          event.context.user = user as CurrentUser;
+        } else {
+          return onNotAuthed(event, "Session is valid but user does not exist");
         }
+      } else {
+        return onNotAuthed(event, "Your did not provide any authorization ");
+      }
+    } catch (e) {
+      return onNotAuthed(event, "Malformed JWT");
     }
+  }
 
-    if (loginSuccesfull && ['/user/new', '/login'].includes(event.path) && event.node.req.method === 'GET') {
-        await sendRedirect(event, '/', 302);
-    }
+  console.log("NOT LOGGED IN 3");
+  if (
+    loginSuccesfull &&
+    ["/user/new", "/login"].includes(event.path) &&
+    event.node.req.method === "GET"
+  ) {
+    await sendRedirect(event, "/", 302);
+  }
 });
 
-
-async function onNotAuthed (
+async function onNotAuthed(
   event: H3Event<EventHandlerRequest>,
-  msg: string = 'You are not logged in. Please log in and try again'
+  msg: string = "You are not logged in. Please log in and try again",
 ): Promise<void> {
-    const isApiUrl = event.path.startsWith('/api/');
+  const isApiUrl = event.path.startsWith("/api/");
 
-    deleteCookie(event, 'authcookie');
+  deleteCookie(event, "authcookie");
 
-    if (!isOpenUrl(event) && isApiUrl) {
-        throw createError({
-            statusCode: 401,
-            statusMessage: msg,
-        });
-    } else if (!isOpenUrl(event) && !isApiUrl) {
-        await sendRedirect(event, '/login', 302);
-    }
+  if (!isOpenUrl(event) && isApiUrl) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: msg,
+    });
+  } else if (!isOpenUrl(event) && !isApiUrl) {
+    await sendRedirect(event, "/login", 302);
+  }
 }
