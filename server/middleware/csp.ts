df5569dad4b0c494015378c7291f73d2f1975d30
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2026-04-10 10:08:17 +0200

    fix: correctly add bug report url from .env to cors headers

--- server/middleware/csp.ts
+++ server/middleware/csp.ts
@@ -1,7 +1,24 @@
 export default defineEventHandler((event) => {
-  setHeader(event, 'Content-Security-Policy', "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://bugs.codecollective.dk;")
-  setHeader(event, 'Cross-Origin-Embedder-Policy', 'require-corp')
-  setHeader(event, 'Cross-Origin-Opener-Policy', 'same-origin')
-  setHeader(event, 'Cross-Origin-Resource-Policy', 'cross-origin')
-  setHeader(event, 'Access-Control-Allow-Headers', 'sentry-trace, baggage')
+  const config = useRuntimeConfig().public;
+  const sentryUrl = new URL(config.sentryDsn);
+
+  if (sentryUrl) {
+    const u = new URL(sentryUrl);
+    const bugReportOrigin = u.origin;
+    setHeader(
+      event,
+      "Content-Security-Policy",
+      `script-src 'self' 'unsafe-inline' 'unsafe-eval' ${bugReportOrigin};`,
+    );
+  } else {
+    setHeader(
+      event,
+      "Content-Security-Policy",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval';",
+    );
+  }
+  setHeader(event, "Cross-Origin-Embedder-Policy", "require-corp");
+  setHeader(event, "Cross-Origin-Opener-Policy", "same-origin");
+  setHeader(event, "Cross-Origin-Resource-Policy", "cross-origin");
+  setHeader(event, "Access-Control-Allow-Headers", "sentry-trace, baggage");
 });
