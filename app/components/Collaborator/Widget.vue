2ad4c9b91f70e6f9cd59bf37fd055166e7b369f9
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2026-04-17 10:35:20 +0200

    fix: lint two components

--- app/components/Collaborator/Widget.vue
+++ app/components/Collaborator/Widget.vue
@@ -4,13 +4,19 @@
       <div class="flex items-center justify-between w-full">
         <CardHeader>
           Collaborators
-          <UTooltip class="ml-2" :ui="{base: 'p-2 text-xs'}">
+          <UTooltip class="ml-2" :ui="{ base: 'p-2 text-xs' }">
             <template #text>
               <p class="mb-3 whitespace-normal break-words">
-                If invited email is already registered on manuscrape, they will recieve access right away. If not, they will get access once they sign up on manuscrape. In that case, the invitation expire after one week.
+                If invited email is already registered on manuscrape, they will
+                recieve access right away. If not, they will get access once
+                they sign up on manuscrape. In that case, the invitation expire
+                after one week.
               </p>
               <p class="whitespace-normal break-words">
-                Manuscrape stores invitations by using one-way encryption on the emails. (aka. hashing). This prevents you from seeing the invited email, however, it makes the invitation system more complaint with GDPR.
+                Manuscrape stores invitations by using one-way encryption on the
+                emails. (aka. hashing). This prevents you from seeing the
+                invited email, however, it makes the invitation system more
+                complaint with GDPR.
               </p>
             </template>
             <UIcon class="text-lg" name="i-heroicons-information-circle" />
@@ -20,7 +26,14 @@
     </template>
     <div class="flex gap-4">
       <UInput type="email" v-model="email" placeholder="Enter email" />
-      <UButton variant="outline" color="blue" icon="i-heroicons-user-plus" :disabled="!validEmail" @click="onSubmit">Add contributor</UButton>
+      <UButton
+        variant="outline"
+        color="blue"
+        icon="i-heroicons-user-plus"
+        :disabled="!validEmail"
+        @click="onSubmit"
+        >Add contributor</UButton
+      >
     </div>
     <CollaboratorList
       :project="project"
@@ -39,97 +52,97 @@
 </template>
 
 <script setup lang="ts">
-  const props = defineProps({
-    project: requireProjectProp,
-  });
-  
-  const toast = useToast();
-  const email = ref('');
-  const openAliasModal = ref(false);
-  const activeCollaborator = ref<Collaborator | null>(null);
-  const validEmail = computed(() => {
-    return isEmail(email.value);
-  });
+const props = defineProps({
+  project: requireProjectProp,
+});
 
-  const { params } = useRoute();
-  const {
-    addCollaborator,
-  } = await useProjects(params);
+const toast = useToast();
+const email = ref("");
+const openAliasModal = ref(false);
+const activeCollaborator = ref<Collaborator | null>(null);
+const validEmail = computed(() => {
+  return isEmail(email.value);
+});
 
+const { params } = useRoute();
+const { addCollaborator } = await useProjects(params);
 
-  const {
-    data: collaboratorsResponse,
-    pending: collaboratorsLoading,
-    refresh: collaboratorsRefresh,
-  } = await useFetch<{ collaborators: Collaborator[] }>(
-    () => `/api/projects/${props.project.id}/collaborators`,
-    {
-      watch: [props.project],
-      method: 'GET',
-      immediate: true,
-      server: true,
-      credentials: 'include',
-      onResponse: async (context) => {
-        if (context.response.status === 401) {
-          await navigateTo('/login', { replace: true })
-        }
-      },
-      onResponseError: async (context) => {
-        if (context.response.status === 401) {
-          await navigateTo('/login', { replace: true })
-        }
+const {
+  data: collaboratorsResponse,
+  pending: collaboratorsLoading,
+  refresh: collaboratorsRefresh,
+} = await useFetch<{ collaborators: Collaborator[] }>(
+  () => `/api/projects/${props.project.id}/collaborators`,
+  {
+    watch: [props.project],
+    method: "GET",
+    immediate: true,
+    server: true,
+    credentials: "include",
+    onResponse: async (context) => {
+      if (context.response.status === 401) {
+        await navigateTo("/login", { replace: true });
       }
-    }
-  );
+    },
+    onResponseError: async (context) => {
+      if (context.response.status === 401) {
+        await navigateTo("/login", { replace: true });
+      }
+    },
+  },
+);
 
-  const collaborators = computed<Collaborator[]>(() => {
-    return collaboratorsResponse.value?.collaborators || [];
-  });
+const collaborators = computed<Collaborator[]>(() => {
+  return collaboratorsResponse.value?.collaborators || [];
+});
 
-  function beginEditAlias(collaborator: Collaborator) {
-    activeCollaborator.value = collaborator;
-    openAliasModal.value = true;
+function beginEditAlias(collaborator: Collaborator) {
+  activeCollaborator.value = collaborator;
+  openAliasModal.value = true;
+}
+
+function onAliasModalClose() {
+  openAliasModal.value = false;
+}
+
+async function onSubmit() {
+  if (!validEmail.value) {
+    toast.add({
+      title: "Invalid email",
+      color: "red",
+      icon: "i-heroicons-exclamation-triangle",
+    });
+    return;
   }
 
-  function onAliasModalClose() {
-    openAliasModal.value = false;
-  }
-
-  async function onSubmit() {
-    if (!validEmail.value) {
-      toast.add({
-        title: 'Invalid email',
-        color: 'red',
-        icon: 'i-heroicons-exclamation-triangle',
-      });
-      return;
-    }
-
-    await addCollaborator(props.project?.id, email.value).then(async (res: Response) => {
+  await addCollaborator(props.project?.id, email.value)
+    .then(async (res: Response) => {
       if ([201, 202].includes(res.status)) {
-        const msg = res.status === 202
-          ? 'Collaborator has been added!'
-          : 'Invitation has been created, and is valid for 7 days.';
+        const msg =
+          res.status === 202
+            ? "Collaborator has been added!"
+            : "Invitation has been created, and is valid for 7 days.";
 
         toast.add({
-          title: 'Success!',
+          title: "Success!",
           description: msg,
-          color: 'green'
+          color: "green",
         });
 
         await collaboratorsRefresh();
       } else {
         throw res;
       }
-    }).catch(async (res: Response) => {
+    })
+    .catch(async (res: Response) => {
       const json = await res.json();
       const msg = getErrMsg(json);
       toast.add({
-        title: 'Error',
+        title: "Error",
         description: msg,
-        color: 'red',
-        icon: 'i-heroicons-exclamation-triangle',
+        color: "red",
+        icon: "i-heroicons-exclamation-triangle",
       });
-    })
-  }
+    });
+}
 </script>
--- app/components/Profile/DeleteUserModal.vue
+++ app/components/Profile/DeleteUserModal.vue
@@ -1,47 +1,54 @@
 <template>
-  <UModal
-    v-bind:model-value="open"
-    v-on:close="onClose"
-  >
+  <UModal v-bind:model-value="open" v-on:close="onClose">
     <UCard>
       <template #header>
         <CardHeader>Delete user account</CardHeader>
       </template>
       <p class="mb-4">
-        Your committed observations will be kept, but no one will be able to see you as the author. If you are alone on some projects, they will be deleted permanently.
+        Your committed observations will be kept, but no one will be able to see
+        you as the author. If you are alone on some projects, they will be
+        deleted permanently.
       </p>
-      <UInput autocomplete="off" class="mb-6" placeholder="Enter password" type="password" v-model="password" />
+      <UInput
+        autocomplete="off"
+        class="mb-6"
+        placeholder="Enter password"
+        type="password"
+        v-model="password"
+      />
       <div class="text-red-500 mb-4" v-if="errorMsg">{{ errorMsg }}</div>
       <div class="w-full text-right">
-        <UButton color="red" @click="onDeleteUserConfirm">Confirm user deletion</UButton>
+        <UButton color="red" @click="onDeleteUserConfirm"
+          >Confirm user deletion</UButton
+        >
       </div>
     </UCard>
   </UModal>
 </template>
 
 <script lang="ts" setup>
-  const props = defineProps({
-    ...requireModalProps
-  });
+const props = defineProps({
+  ...requireModalProps,
+});
 
-  const password = ref('');
-  const toast = useToast();
-  const { deleteUser } = await useAuth();
-  const { refreshUser } = await useUser();
-  const errorMsg = ref<null | string>(null)
+const password = ref("");
+const toast = useToast();
+const { deleteUser } = await useAuth();
+const { refreshUser } = await useUser();
+const errorMsg = ref<null | string>(null);
 
-  async function onDeleteUserConfirm() {
-    try {
-      props.onClose()
-      await deleteUser(password.value);
-      toast.add({
-        title: 'Success',
-        description: 'Your user was successfully deleted'
-      });
-      navigateTo('/login')
-      await refreshUser();
-    } catch(err) {
-      errorMsg.value = getErrMsg(err);
-    }
+async function onDeleteUserConfirm() {
+  try {
+    props.onClose();
+    await deleteUser(password.value);
+    toast.add({
+      title: "Success",
+      description: "Your user was successfully deleted",
+    });
+    navigateTo("/login");
+    await refreshUser();
+  } catch (err) {
+    errorMsg.value = getErrMsg(err);
   }
+}
 </script>
