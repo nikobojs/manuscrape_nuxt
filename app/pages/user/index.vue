c61f868f31e6b9ce64ecd59ae9a26ef0a50bde8c
Nikolaj Fabricius-Bjerre <nfb@codecollective.dk>
2026-04-29 20:58:51 +0200

    fix: settings redirecting to project view on initial page load

--- app/pages/user/index.vue
+++ app/pages/user/index.vue
@@ -11,7 +11,7 @@
 
 <script lang="ts" setup>
 const { ensureLoggedIn } = await useAuth();
-await ensureLoggedIn();
 const { user } = await useUser();
+await ensureLoggedIn();
 const { isElectron } = useDevice();
 </script>
